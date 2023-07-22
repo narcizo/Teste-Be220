161b0c7a2ec68ceed3e6aa53b38aef763a6d538e
narcizo <narcizo.gabriel2@gmail.com>
2023-07-22 00:12:41 -0300

    Adicionando input nos cards

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-list',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-list.component.scss'],
 })
 export class CardListComponent  implements OnInit {
+  @Input() title:string = ""
 
   constructor() { }
 
